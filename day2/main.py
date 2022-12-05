6a3f00682d571607ea083b9462617e09be8f2c68
Darren Christopher <a@atsui.moe>
2022-12-06 04:49:47 +0900

    Day 2 Solve

--- day2/main.py
+++ day2/main.py
@@ -1,7 +1,33 @@
 # Base score rewards: x = 1pt, y = 2pt, z = 3pt
 # Result score rewards: Lose = 0pt, Draw = 3pt, Win = 6pt
 
-scoreAwards = {"a": {"x": 4, "y": 7, "z": 3},
-               "b": {"x": 1, "y": 5, "z": 9},
-               "c": {"x": 7, "y": 2, "z": 6}}
+scoreAwardsPart1 = {"A": {"X": 4, "Y": 8, "Z": 3},
+                    "B": {"X": 1, "Y": 5, "Z": 9},
+                    "C": {"X": 7, "Y": 2, "Z": 6}}
 
+# Base score rewards: x = 1pt, y = 2pt, z = 3pt
+# Result score rewards: Lose = 0pt, Draw = 3pt, Win = 6pt
+# Result points only awarded with the following win conditions:
+#   X = Lose, Y = Draw = Z = Win
+
+scoreAwardsPart2 = {"A": {"X": 3, "Y": 4, "Z": 8},
+                    "B": {"X": 1, "Y": 5, "Z": 9},
+                    "C": {"X": 2, "Y": 6, "Z": 7}}
+
+file = open("input/adventOfCodeDay2Input.txt", "r")
+currentFileReadline = file.readline()
+
+elfCaloriesList: list[int] = []
+totalScorePart1: int = 0
+totalScorePart2: int = 0
+
+while currentFileReadline != '':
+    currentFileReadlineArray: list[str] = currentFileReadline.split()
+    totalScorePart1 += scoreAwardsPart1[currentFileReadlineArray[0]][currentFileReadlineArray[1]]
+    totalScorePart2 += scoreAwardsPart2[currentFileReadlineArray[0]][currentFileReadlineArray[1]]
+
+    currentFileReadline = file.readline()
+
+
+print("AoC 2022 Day 2 Part 1 Solution is", totalScorePart1)
+print("AoC 2022 Day 2 Part 2 Solution is", totalScorePart2)
